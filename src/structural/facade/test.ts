9ca1a8572412bbd82284b20e7f74806c38b085f0
saloghubsergi-png <saloghubsergi@gmail.com>
2026-05-06 15:50:28 +0300

    Update test.ts

--- src/structural/facade/test.ts
+++ src/structural/facade/test.ts
@@ -1,1 +1,24 @@
+class CPU {
+  start() {
+    console.log("CPU started");
+  }
+}
 
+class Memory {
+  load() {
+    console.log("Memory loaded");
+  }
+}
+
+class ComputerFacade {
+  private cpu = new CPU();
+  private memory = new Memory();
+
+  start() {
+    this.cpu.start();
+    this.memory.load();
+    console.log("Computer started");
+  }
+}
+
+export {};
