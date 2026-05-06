720047a372fb98ad692d82e7740a32a40683562b
saloghubsergi-png <saloghubsergi@gmail.com>
2026-05-06 15:46:24 +0300

    Update test.ts

--- src/creational/singleton/test.ts
+++ src/creational/singleton/test.ts
@@ -1,1 +1,18 @@
+class Database {
+  private static instance: Database;
 
+  private constructor() {}
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  connect() {
+    console.log("Connected to DB");
+  }
+}
+
+export {};
