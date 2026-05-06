2fe93da05ebc8c9f2d734225de6c811126125df2
saloghubsergi-png <saloghubsergi@gmail.com>
2026-05-06 15:52:01 +0300

    Update test.ts

--- src/behavioral/strategy/test.ts
+++ src/behavioral/strategy/test.ts
@@ -1,1 +1,25 @@
+interface Strategy {
+  execute(a: number, b: number): number;
+}
 
+class Add implements Strategy {
+  execute(a: number, b: number) {
+    return a + b;
+  }
+}
+
+class Multiply implements Strategy {
+  execute(a: number, b: number) {
+    return a * b;
+  }
+}
+
+class Context {
+  constructor(private strategy: Strategy) {}
+
+  run(a: number, b: number) {
+    return this.strategy.execute(a, b);
+  }
+}
+
+export {};
