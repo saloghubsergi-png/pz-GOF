fbdebb392ad6c8e5e55910f6626651cc2cce8ab2
saloghubsergi-png <saloghubsergi@gmail.com>
2026-05-06 15:53:12 +0300

    Update test.ts

--- src/behavioral/observer/test.ts
+++ src/behavioral/observer/test.ts
@@ -1,1 +1,13 @@
+class Subject {
+  private observers: Function[] = [];
 
+  subscribe(fn: Function) {
+    this.observers.push(fn);
+  }
+
+  notify(data: string) {
+    this.observers.forEach(fn => fn(data));
+  }
+}
+
+export {};
