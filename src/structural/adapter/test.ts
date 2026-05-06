64d4974872c9a6717b5b989cbede3cd239f00ddd
saloghubsergi-png <saloghubsergi@gmail.com>
2026-05-06 15:46:57 +0300

    Update test.ts

--- src/structural/adapter/test.ts
+++ src/structural/adapter/test.ts
@@ -1,1 +1,15 @@
+class OldSystem {
+  oldRequest() {
+    return "Old system data";
+  }
+}
 
+class Adapter {
+  constructor(private oldSystem: OldSystem) {}
+
+  request() {
+    return this.oldSystem.oldRequest();
+  }
+}
+
+export {};
