68afcae7910510a684d9d48fc31002979c33e606
saloghubsergi-png <saloghubsergi@gmail.com>
2026-05-06 15:45:50 +0300

    Update test.ts

--- src/creational/factory-method/test.ts
+++ src/creational/factory-method/test.ts
@@ -1,1 +1,25 @@
+interface Transport {
+  deliver(): void;
+}
 
+class Truck implements Transport {
+  deliver() {
+    console.log("Delivery by truck");
+  }
+}
+
+class Ship implements Transport {
+  deliver() {
+    console.log("Delivery by ship");
+  }
+}
+
+class TransportFactory {
+  static create(type: string): Transport {
+    if (type === "truck") return new Truck();
+    if (type === "ship") return new Ship();
+    throw new Error("Unknown type");
+  }
+}
+
+export {};
